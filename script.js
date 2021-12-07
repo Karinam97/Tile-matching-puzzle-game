12a0efac3a833671625577e369f34a482f93e014
karin <karin@LAPTOP-DU9V1KQJ>
2021-12-07 07:23:45 +0000

    flipping the card

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('hello');
+const tiles = document.getElementById('tile');
+
+
+tiles.addEventListener('click', flipcard);
+
+
+function flipcard(){
+  tiles.style.transform = "rotateY(180deg)";
+}
