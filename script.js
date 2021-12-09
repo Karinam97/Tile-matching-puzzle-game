d74d42b5863d5114eeab494ae94e6ab21dd2253c
karin <karin@LAPTOP-DU9V1KQJ>
2021-12-09 11:29:45 +0000

    functionality of removing cards when match

--- script.js
+++ script.js
@@ -12,37 +12,40 @@ one color to two cards and also add classname of that color*/
 /*when event is triggered by clicking on the card
 also adds classname to open
 applies to multiple cards*/
-cards.forEach(card => {
-
-  card.addEventListener('click', event => {
+cards.forEach(card => { /*this loops through each card without event listener*/
+  card.addEventListener('click', () => { /*event listener is added to each of the card*/
 
     card.style.transform = "rotateY(180deg)"; /*once clicked rotates card*/
     card.classList.add('open');/*adds classname 'open'*/
     opened_cards.push(card);/*once card is open its pushed to opened_cards array*/
 
     /*checking if there is more than 2 opened cards*/
-    if(opened_cards.length > 2){
+    if(opened_cards.length === 2){
 
       /*
         ******************************************************
         it has to check if two cards are the same, if yes it has to remove elements
         from document,
+
+        document.querySelector('.flip-card.red').remove()
+
         if no, then just rotates them back to the same position
       */
 
-      let open = opened_cards.every(el => el.className === 'blue');
-      console.log(open)
-      if(open){
+      if(opened_cards[0].className === 'flip-card blue open' && opened_cards[1].className === 'flip-card blue open'){
         console.log('im here **********')
-        removeCards();
+        opened_cards.forEach(el => el.remove('.flip-card.blue.open'));
+        opened_cards = [];
       }else{
         opened_cards.forEach(el => {
           el.style.transform = "rotateY(0)";/*rotates to the same position*/
           el.classList.remove('open'); /*when rotates back it removes 'open' class*/
+
         })
+        opened_cards = []; /*important to remove element from this list when they flip back*/
       }
 
-      opened_cards = []; /*important to remove element from this list when they flip back*/
+
       }
     });
   });
