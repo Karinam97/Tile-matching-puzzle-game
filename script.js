eb9a3c383bd7005077a87e3240a2410a865f666d
karin <karin@LAPTOP-DU9V1KQJ>
2021-12-13 11:19:43 +0000

    code refactored and now has better transition

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.querySelectorAll('.flip-card').forEach(card => {
   cards.push(card);
 });
 let opened_cards = [];
+let matched_cards;
 /*randomly assign colors to cards
 one color to two cards and also add classname of that color*/
 
@@ -15,41 +16,49 @@ applies to multiple cards*/
 cards.forEach(card => { /*this loops through each card without event listener*/
   card.addEventListener('click', () => { /*event listener is added to each of the card*/
 
-    card.style.transform = "rotateY(180deg)"; /*once clicked rotates card*/
-    card.classList.add('open');/*adds classname 'open'*/
-    opened_cards.push(card);/*once card is open its pushed to opened_cards array*/
+    //allows to open only two cards
 
-    /*checking if there is more than 2 opened cards*/
-    if(opened_cards.length === 2){
+      card.style.transform = "rotateY(180deg)"; /*once clicked rotates card*/
+      card.classList.add('open');/*adds classname 'open'*/
+      opened_cards.push(card);/*once card is open its pushed to opened_cards array*/
 
-      /*
-        ******************************************************
-        it has to check if two cards are the same, if yes it has to remove elements
-        from document,
+      console.log(opened_cards.length);
 
-        document.querySelector('.flip-card.red').remove()
+      /*checking if there is more than 2 opened cards*/
+      if(opened_cards.length === 2){
+        console.log('im here two cards open')
+        setTimeout(match, 1500) //executes code only when the timer expires
 
-        if no, then just rotates them back to the same position
-      */
-
-      if(opened_cards[0].className === 'flip-card blue open' && opened_cards[1].className === 'flip-card blue open'){
-        console.log('im here **********')
-        opened_cards.forEach(el => el.remove('.flip-card.blue.open'));
-        opened_cards = [];
-      }else{
-        opened_cards.forEach(el => {
-          el.style.transform = "rotateY(0)";/*rotates to the same position*/
-          el.classList.remove('open'); /*when rotates back it removes 'open' class*/
-
-        })
-        opened_cards = []; /*important to remove element from this list when they flip back*/
-      }
+        }
 
 
-      }
     });
   });
-/*flip card back and remove class name 'open'*/
 
+function match(){
+  matched_cards = opened_cards.every(card => card.classList.contains('blue'))
+  if(matched_cards){
 
-//need to write a loop
+    opened_cards.forEach(el => {
+      //el.style.transform = "rotateY(0deg)";
+      el.classList.add('matched');
+      //el.style.display = 'none';
+    });
+
+    opened_cards.forEach(el =>{
+
+      el.classList.remove('open');
+    })
+
+  }
+  else{
+      console.log('doesn\'t match');
+      setTimeout(opened_cards.forEach(el => {
+        //el.style.transform = "rotateY(0deg)";
+        el.style.transform = "rotateY(0)";/*rotates to the same position*/
+        el.classList.remove('open'); /*when rotates back it removes 'open' class*/
+        //el.style.display = 'none';
+      }),1000);
+    }
+  opened_cards = [];
+}
